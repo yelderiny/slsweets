3209fd38179d6bc0953e1fffc5360cfede7ad21c
yelderiny <youssefelderiny@gmail.com>
2024-10-15 19:28:48 +0400

    Generated content and styled the faq section of the home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Header from '@/components/header.client';
 import MobileNav from '@/components/mobile-nav.client';
 import Link from 'next/link';
 import {speciality} from '@/types/img';
+import {faq} from '@/types/faq';
 
 export default function Home() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -50,6 +51,56 @@ export default function Home() {
             description: 'Chocolate Chip Muffins',
             price: 35,
             overrides: ''
+        }
+    ];
+    const faqs: faq[] = [
+        {
+            question: "What are your business hours?",
+            answer: "We accept orders 24/7. Once your order is confirmed, we’ll begin preparing it right away."
+        },
+        {
+            question: "Where is your bakery located?",
+            answer: "We are located in Mirdif, Dubai, at Labban Manor. You can find us on maps."
+        },
+        {
+            question: "Do you offer custom cakes and desserts?",
+            answer: "Yes, we do! For custom orders, we recommend contacting us through Instagram to discuss what’s possible."
+        },
+        {
+            question: "How far in advance should I place an order?",
+            answer: "Please place your order at least one day in advance."
+        },
+        {
+            question: "Can I place an order online or by phone?",
+            answer: "All orders are placed via Instagram direct messages (DMs)."
+        },
+        {
+            question: "Do you offer delivery? Which areas do you serve?",
+            answer: "Yes, we deliver to both Dubai and Sharjah."
+        },
+        {
+            question: "Is there a minimum order requirement for delivery?",
+            answer: "Yes, there is a minimum order of AED 30 for delivery."
+        },
+        {
+            question: "Do you offer catering services for events?",
+            answer: "Absolutely! We can work with you to customize your order to suit your event’s needs."
+        },
+        {
+            question: "What payment methods do you accept?",
+            answer: "We currently accept cash payments."
+        },
+        {
+            question: "Can I place an order for same-day delivery, or do I need to pre-order?",
+            answer: "Cheesecake orders must be pre-ordered, but for other menu items, same-day delivery may be possible if the order is placed a few hours in advance."
+        },
+        {
+            question: "What is the delivery fee for my area?",
+            answer: "The delivery fee is AED 10 for both Dubai and Sharjah."
+        },
+        {
+            question: "Where can I pick up my order?",
+            answer: "You can pick up your order from our location in Mirdif, Street 69C, Compound 39, Villa 2."
         }
     ];
 
@@ -165,9 +216,10 @@ export default function Home() {
                                         width={item.width}
                                         height={item.height}
                                     />
-                                    <p className='text-base mt-2 uppercase'>{item.description}</p>
-                                    <p className='text-xs mt-4'>
-                                        from <strong className='text-base text-secondary'>AED {item.price}</strong>
+                                    <p className='text-base mt-2 uppercase lg:text-lg'>{item.description}</p>
+                                    <p className='text-xs mt-4 lg:text-sm'>
+                                        from <strong
+                                        className='text-base text-secondary lg:text-lg'>AED {item.price}</strong>
                                     </p>
                                 </div>
                             ))
@@ -176,13 +228,28 @@ export default function Home() {
                 </section>
 
                 {/* faq */}
-                <section id='faq' className='container'>
+                <section id='faq' className='container space-y-4'>
                     <h2 className='title text-center'>
                         <span className='relative'>Faq
                             <span
                                 className='absolute inset-x-0 bottom-2 h-2 bg-secondary -z-10 opacity-30 lg:bottom-4'/>
                         </span>
                     </h2>
+                    <p className='text-sm text-center max-w-prose mx-auto lg:text-base'>
+                        If you have any questions, check out this list to see if it&apos;s already been answered. In
+                        the case that it hasn&apos;t, feel free to reach out to us on Instagram and we&apos;ll get
+                        back to you as soon as possible. We&apos;re always happy to help.
+                    </p>
+                    <div className='space-y-4 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-4 xl:grid-cols-3'>
+                        {
+                            faqs.map((item, index) => (
+                                <div key={index} className='p-8 rounded-md shadow-md bg-accent'>
+                                    <h3 className='text-base font-bold mb-1'>{item.question}</h3>
+                                    <p className='text-sm'>{item.answer}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </section>
 
                 {/* contact us */}
