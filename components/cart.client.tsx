049d653cf0380319a79cce466e2b11dc5b0c94b8
yelderiny <youssefelderiny@gmail.com>
2024-11-07 21:35:29 +0400

    Organizational changes + padding changed to margin

--- components/cart.client.tsx
+++ components/cart.client.tsx
@@ -1,19 +1,18 @@
-'use client'
+'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import {useAppContext} from '@/app/context';
 import {LiaTimesSolid} from 'react-icons/lia';
-import {IoIosAdd, IoIosRemove} from "react-icons/io";
-import Link from "next/link";
+import {IoIosAdd, IoIosRemove} from 'react-icons/io';
 
 
 const Cart = () => {
-    const {isCartOpen, toggleCart, cart, setCart} = useAppContext();
+    const { isCartOpen, toggleCart, cart, setCart } = useAppContext();
 
-    const handleQuantityChange = (itemIndex: number, delta: number) =>
-        setCart(prevItems => prevItems.map((currItem, currIndex) =>
-            currIndex === itemIndex ? {...currItem, quantity: Math.max(1, currItem.quantity + delta)} : currItem))
-
+    const handleQuantityChange = (current: number, delta: number) =>
+        setCart(prevItems => prevItems.map((item, index) =>
+            index === current ? { ...item, quantity: Math.max(1, item.quantity + delta) } : item));
 
     return (
         <div
@@ -23,63 +22,55 @@ const Cart = () => {
             md:left-auto md:w-[45%] lg:w-[40%]
         `}
         >
-            <div className='flex justify-between items-center px-4 pt-6 pb-2 text-3xl'>
+            <div className='flex justify-between items-center mx-4 mt-6 mb-2 text-3xl'>
                 <h2>Cart</h2>
-                <button
-                    className='text-text'
-                    aria-label='close menu'
-                    onClick={toggleCart}
-                >
+                <button className='text-text' aria-label='close menu' onClick={toggleCart}>
                     <LiaTimesSolid/>
                 </button>
             </div>
             <hr className='mx-4 border-t border-gray-300'/>
             {
                 cart.length === 0 ?
-                    <div className="flex items-center justify-center h-full text-xl">
+                    <div className='flex items-center justify-center h-full text-xl'>
                         Your cart is empty
                     </div> :
-                    <div className="flex-1 overflow-y-auto pb-20">
+                    <div className='mb-20'>
                         {
                             cart.map((item, index) => (
-                                <div key={index} className="px-8 py-4 ">
-                                    <div className="flex justify-between">
-                                        <div className="w-2/3">
-                                            <p className="capitalize font-medium text-base">
-                                                {item.name}
-                                            </p>
-                                            <p className="text-xs">
-                                                {item.description}
-                                            </p>
-                                            <p className="text-xs pt-2">
-                                                AED {item.price}
-                                            </p>
+                                <div key={index} className='mx-4 my-4'>
+                                    <div className='flex justify-between'>
+                                        <div className='w-2/3'>
+                                            <p className='capitalize font-medium text-base'>{item.name}</p>
+                                            <p className='text-xs mt-1'>{item.description}</p>
+                                            <p className='text-xs mt-4'>AED {item.price}</p>
                                         </div>
+                                        {/*todo: transition the underline rather than the italic on hover*/}
                                         <p
-                                            className="underline flex justify-center items-center
-                                            hover:italic transition duration-500 ease-in-out text-xs"
-                                            onClick={() => {
-                                                setCart(prevItems => prevItems.filter((_, i) => i !== index))
-                                            }}>
+                                            className='flex justify-center items-center cursor-pointer underline hover:italic
+                                            transition duration-500 ease-in-out text-xs'
+                                            onClick={() => setCart(prevItems => prevItems.filter((_, i) => i !== index))}
+                                        >
                                             Remove
                                         </p>
                                     </div>
-                                    <div className="flex items-center justify-around mx-16">
-                                        <IoIosRemove onClick={() => handleQuantityChange(index, -1)}/>
-                                        <div>
-                                            {item.quantity}
-                                        </div>
-                                        <IoIosAdd onClick={() => handleQuantityChange(index, 1)}/>
+                                    <div className='flex justify-center items-center gap-8'>
+                                        <IoIosRemove
+                                            className='cursor-pointer'
+                                            onClick={() => handleQuantityChange(index, -1)}
+                                        />
+                                        <div>{item.quantity}</div>
+                                        <IoIosAdd
+                                            className='cursor-pointer'
+                                            onClick={() => handleQuantityChange(index, 1)}
+                                        />
                                     </div>
-                                    <hr className='mx-8 border-t border-gray-200 my-2'/>
+                                    <hr className='mx-8 my-2 border-t border-gray-200'/>
                                 </div>
                             ))
                         }
-                        <div className="flex fixed bottom-0 left-0 w-full py-8 justify-center">
-                            <Link href="/checkout">
-                                <button className="btn btn-primary">
-                                    Checkout
-                                </button>
+                        <div className='fixed bottom-0 left-0 flex justify-center w-full my-8'>
+                            <Link href='/checkout'>
+                                <button className='btn btn-primary'>Checkout</button>
                             </Link>
                         </div>
                     </div>
